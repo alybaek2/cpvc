f05677222508a9c7379f7558a119fc282db28284
adrian <alybaek2@gmail.com>
2019-11-17 13:39:54 -0800

    Updating code coverage output to HTML.

--- buildkite/build_and_test.bat
+++ buildkite/build_and_test.bat
@@ -13,4 +13,4 @@ fsutil file createnew roms\basic6128.rom 16384
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /platform:x64 cpvc-test\bin\x64\Release\cpvc-test.dll
 x64\Release\cpvc-core-test.exe
 
-OpenCppCoverage.exe --modules cpvc-core-test --export_type cobertura --sources cpvc "x64\Debug\cpvc-core-test.exe"
+OpenCppCoverage.exe --modules cpvc-core-test --export_type html:cpvc-core-coverage --sources cpvc "x64\Debug\cpvc-core-test.exe"
