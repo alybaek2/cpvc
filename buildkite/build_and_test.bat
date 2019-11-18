34f76e0fd96651941d0acf905250218a4818a869
adrian <alybaek2@gmail.com>
2019-11-17 17:52:18 -0800

    Adding Debug build back.

--- buildkite/build_and_test.bat
+++ buildkite/build_and_test.bat
@@ -8,7 +8,7 @@ del roms\basic6128.rom
 fsutil file createnew roms\basic6128.rom 16384
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" cpvc.sln /t:Rebuild /p:Configuration=Release /p:Platform="x64"
-REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" cpvc.sln /t:Rebuild /p:Configuration=Debug /p:Platform="x64"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" cpvc.sln /t:Rebuild /p:Configuration=Debug /p:Platform="x64"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /platform:x64 cpvc-test\bin\x64\Release\cpvc-test.dll
 x64\Release\cpvc-core-test.exe
