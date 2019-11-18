986074a3222a744479d30e9400dbf3400473c5f9
adrian <alybaek2@gmail.com>
2019-11-18 13:21:46 -0800

    Testing how Coveralls uploader deals with no commit information.

--- buildkite/build_and_test.bat
+++ buildkite/build_and_test.bat
@@ -1,5 +1,6 @@
 "C:\Tools\NuGet\nuget.exe" restore
 
+REM Generate dummy rom files.
 del roms\os6128.rom
 fsutil file createnew roms\os6128.rom 16384
 del roms\amsdos6128.rom
@@ -7,9 +8,11 @@ fsutil file createnew roms\amsdos6128.rom 16384
 del roms\basic6128.rom
 fsutil file createnew roms\basic6128.rom 16384
 
+REM Build Debug and Release (x64).
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" cpvc.sln /t:Rebuild /p:Configuration=Release /p:Platform="x64"
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" cpvc.sln /t:Rebuild /p:Configuration=Debug /p:Platform="x64"
 
+REM Run unit tests.
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /platform:x64 cpvc-test\bin\x64\Release\cpvc-test.dll
 x64\Release\cpvc-core-test.exe
 
@@ -32,4 +35,4 @@ dir /b /s /a:-D coverage-report-xml\*.xml > files.txt
 "C:\Tools\CorrectCase\CorrectCase.exe" files.txt tokens.txt
 
 REM Upload to Coveralls!
-"C:\Tools\Coveralls.NET\csmacnz.Coveralls.exe" -i coverage-report-xml --reportgenerator --useRelativePaths --commitAuthor=%BUILDKITE_BUILD_CREATOR% --commitId %BUILDKITE_COMMIT% --commitBranch %BUILDKITE_BRANCH% --commitEmail %BUILDKITE_BUILD_CREATOR_EMAIL% --jobId=%BUILDKITE_JOB_ID%
+"C:\Tools\Coveralls.NET\csmacnz.Coveralls.exe" -i coverage-report-xml --reportgenerator --useRelativePaths --jobId=%BUILDKITE_JOB_ID%
