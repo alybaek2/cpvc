cad4e887ff3986228f06a5b5d637d37c342371af
adrian <alybaek2@gmail.com>
2019-11-18 19:23:03 -0800

    Adding console logging for backup and restore.

--- buildkite/backup.bat
+++ buildkite/backup.bat
@@ -1,3 +1,4 @@
 set BUILDTMP=%NASTMP%\BuildKite\%BUILDKITE_JOB_ID%
+echo Backing up %1 to %BUILDTMP%...
 
 xcopy .\%1 %BUILDTMP%\%1 /y /s /h /i
--- buildkite/restore.bat
+++ buildkite/restore.bat
@@ -1,3 +1,4 @@
 set BUILDTMP=%NASTMP%\BuildKite\%BUILDKITE_JOB_ID%
+echo Restoring %1 to %BUILDTMP%...
 
 xcopy %BUILDTMP%\%1 .\%1 /y /s /h /i
