e04198928820fc66bcdd599fef0fd318400461b6
adrian <alybaek2@gmail.com>
2019-11-23 16:50:08 -0800

    Updated parameters passed to coveralls.

--- buildkite/coverage.bat
+++ buildkite/coverage.bat
@@ -29,7 +29,7 @@ REM Escape the double quotes.
 set COMMIT_MESSAGE=%COMMIT_MESSAGE:"=\"%
 
 REM Upload to Coveralls!
-%BUILD_COVERALLS% -i coverage-report-xml --reportgenerator --useRelativePaths --commitMessage "%COMMIT_MESSAGE%" --commitAuthor=%BUILDKITE_BUILD_CREATOR% --commitId %BUILDKITE_COMMIT% --commitBranch %BUILDKITE_BRANCH% --commitEmail %BUILDKITE_BUILD_CREATOR_EMAIL% --jobId=%BUILDKITE_JOB_ID%
+%BUILD_COVERALLS% -i coverage-report-xml --reportgenerator --useRelativePaths --serviceName buildkite --serviceNumber %BUILDKITE_BUILD_NUMBER% --commitMessage "%COMMIT_MESSAGE%" --commitAuthor=%BUILDKITE_BUILD_CREATOR% --commitId %BUILDKITE_COMMIT% --commitBranch %BUILDKITE_BRANCH% --commitEmail %BUILDKITE_BUILD_CREATOR_EMAIL% --jobId=%BUILDKITE_JOB_ID%
 
 REM Upload to Codecov
 %BUILD_CODECOV% --branch %BUILDKITE_BRANCH% --build %BUILDKITE_BUILD_NUMBER% --sha %BUILDKITE_COMMIT% --file "cpvc-core-coverage.xml cpvc-coverage.xml"
