878029448d1f4a74ff3167c022cb42818d63ed7b
adrian <alybaek2@gmail.com>
2020-01-23 20:22:07 -0800

    Ensure to return an error if either of the tests fail.

--- buildkite/test.bat
+++ buildkite/test.bat
@@ -1,6 +1,9 @@
 call .\buildkite\restore.bat cpvc-test\bin
 call .\buildkite\restore.bat x64
 
-REM Run unit tests.
+REM Run C# unit tests, and quit immediately if there's an error.
 %BUILD_NUNIT% "cpvc-test\bin\x64\Release\cpvc-test.dll"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Run C++ unit tests.
 x64\Release\cpvc-core-test.exe
