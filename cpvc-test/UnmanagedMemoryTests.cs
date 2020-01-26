05830a8c48b040133a78190273ff7b56fdf1266e
adrian <alybaek2@gmail.com>
2020-01-26 09:37:09 -0800

    Adding test for UnmanagedMemory.

---
+++ cpvc-test/UnmanagedMemoryTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CPvC.Test
+{
+    public class UnmanagedMemoryTests
+    {
+        [Test]
+        public void DisposeTwice()
+        {
+            // Setup
+            UnmanagedMemory um = new UnmanagedMemory(1, 0);
+
+            // Act
+            um.Dispose();
+
+            // Verify
+            Assert.DoesNotThrow(() => um.Dispose());
+        }
+    }
+}
