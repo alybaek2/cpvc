aed87623bfd45330fbb41d2fb31af9bbdf168b7e
adrian <alybaek2@gmail.com>
2020-02-17 21:52:45 -0800

    Added unit tests for ViewModelCommand.

---
+++ cpvc-test/ViewModelCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using static CPvC.Test.TestHelpers;
+
+namespace CPvC.Test
+{
+    public class ViewModelCommandTests
+    {
+        [TestCase(false)]
+        [TestCase(true)]
+        public void Execute(bool canExecute)
+        {
+            // Setup
+            bool executeCalled = false;
+            ViewModelCommand command = new ViewModelCommand(
+                p => { executeCalled = true; },
+                p => { return canExecute; });
+
+            // Act
+            command.Execute(null);
+
+            // Verify
+            Assert.True(executeCalled);
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void CanExecute(bool canExecute)
+        {
+            // Setup
+            ViewModelCommand command = new ViewModelCommand(
+                p => { },
+                p => { return canExecute; });
+
+            // Act
+            bool result = command.CanExecute(null);
+
+            // Verify
+            Assert.AreEqual(canExecute, result);
+        }
+    }
+}
