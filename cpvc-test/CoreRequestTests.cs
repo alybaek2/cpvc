a1466f80c704960c750baa395112dca2845c653d
adrian <alybaek2@gmail.com>
2020-03-14 22:40:44 -0700

    Added tests for CoreReqests object.

---
+++ cpvc-test/CoreRequestTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPvC.Test
+{
+    public class CoreRequestTests
+    {
+        [TestCase(false)]
+        [TestCase(true)]
+        public void LoadDisc(bool eject)
+        {
+            // Setup
+            byte[] bytes = new byte[] { 0x01, 0x02 };
+
+            // Act
+            CoreRequest request = CoreRequest.LoadDisc(1, eject ? null : bytes);
+
+            // Verify
+            Assert.AreEqual(CoreRequest.Types.LoadDisc, request.Type);
+            Assert.AreEqual(1, request.Drive);
+
+            if (eject)
+            {
+                Assert.IsNull(request.MediaBuffer.GetBytes());
+            }
+            else
+            {
+                Assert.AreEqual(bytes, request.MediaBuffer.GetBytes());
+            }
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void LoadTape(bool eject)
+        {
+            // Setup
+            byte[] bytes = new byte[] { 0x01, 0x02 };
+
+            // Act
+            CoreRequest request = CoreRequest.LoadTape(eject ? null : bytes);
+
+            // Verify
+            Assert.AreEqual(CoreRequest.Types.LoadTape, request.Type);
+
+            if (eject)
+            {
+                Assert.IsNull(request.MediaBuffer.GetBytes());
+            }
+            else
+            {
+                Assert.AreEqual(bytes, request.MediaBuffer.GetBytes());
+            }
+        }
+    }
+}
