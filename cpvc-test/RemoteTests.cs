3cad03ec8010bdbf980e2bb17e212f7ee4ffd94a
adrian <alybaek2@gmail.com>
2020-05-03 08:32:10 -0700

    Additional Remote unit tests.

--- cpvc-test/RemoteTests.cs
+++ cpvc-test/RemoteTests.cs
@@ -7,6 +7,40 @@ namespace CPvC.Test
     public class RemoteTests
     {
         [Test]
+        public void SendSelectMachine()
+        {
+            // Setup
+            Mock<IConnection> mockConnection = new Mock<IConnection>();
+            mockConnection.Setup(c => c.SendMessage(It.IsAny<byte[]>()));
+            mockConnection.SetupGet(c => c.IsConnected).Returns(true);
+
+            Remote remote = new Remote(mockConnection.Object);
+
+            // Act
+            remote.SendSelectMachine("abc");
+
+            // Verify
+            mockConnection.Verify(c => c.SendMessage(new byte[] { 0x02, 0x03, 0x00, 0x00, 0x00, 0x61, 0x62, 0x63 }));
+        }
+
+        [Test]
+        public void SendRequestAvailableMachines()
+        {
+            // Setup
+            Mock<IConnection> mockConnection = new Mock<IConnection>();
+            mockConnection.Setup(c => c.SendMessage(It.IsAny<byte[]>()));
+            mockConnection.SetupGet(c => c.IsConnected).Returns(true);
+
+            Remote remote = new Remote(mockConnection.Object);
+
+            // Act
+            remote.SendRequestAvailableMachines();
+
+            // Verify
+            mockConnection.Verify(c => c.SendMessage(new byte[] { 0x05 }));
+        }
+
+        [Test]
         public void SendCoreAction()
         {
             // Setup
@@ -21,6 +55,40 @@ namespace CPvC.Test
 
             // Verify
             mockConnection.Verify(c => c.SendMessage(new byte[] { 0x03, 0x01, 0x67, 0x45, 0x23, 0x01, 0x00, 0x00, 0x00, 0x00, 58, 0xFF }));
+        }
+
+        [Test]
+        public void SendCoreRequest()
+        {
+            // Setup
+            Mock<IConnection> mockConnection = new Mock<IConnection>();
+            mockConnection.Setup(c => c.SendMessage(It.IsAny<byte[]>()));
+            mockConnection.SetupGet(c => c.IsConnected).Returns(true);
+
+            Remote remote = new Remote(mockConnection.Object);
+
+            // Act
+            remote.SendCoreRequest(CoreRequest.KeyPress(Keys.A, true));
+
+            // Verify
+            mockConnection.Verify(c => c.SendMessage(new byte[] { 0x07, 0x01, 58, 0xFF }));
+        }
+
+        [Test]
+        public void SendName()
+        {
+            // Setup
+            Mock<IConnection> mockConnection = new Mock<IConnection>();
+            mockConnection.Setup(c => c.SendMessage(It.IsAny<byte[]>()));
+            mockConnection.SetupGet(c => c.IsConnected).Returns(true);
+
+            Remote remote = new Remote(mockConnection.Object);
+
+            // Act
+            remote.SendName("abc");
+
+            // Verify
+            mockConnection.Verify(c => c.SendMessage(new byte[] { 0x06, 0x03, 0x00, 0x00, 0x00, 0x61, 0x62, 0x63 }));
         }
 
         [Test]
@@ -48,7 +116,30 @@ namespace CPvC.Test
         }
 
         [Test]
-        public void NewMessage()
+        public void SendPing()
+        {
+            // Setup
+            Mock<IConnection> mockConnection = new Mock<IConnection>();
+            mockConnection.Setup(c => c.SendMessage(It.IsAny<byte[]>()));
+            mockConnection.SetupGet(c => c.IsConnected).Returns(true);
+
+            Remote remote = new Remote(mockConnection.Object);
+
+            // Act
+            remote.SendPing(true, 0x0123456789abcdef);
+
+            // Verify
+            byte[] expectedMsg = new byte[] {
+                0x04,
+                0xff,
+                0xef, 0xcd, 0xab, 0x89, 0x67, 0x45, 0x23, 0x01
+            };
+
+            mockConnection.Verify(c => c.SendMessage(expectedMsg));
+        }
+
+        [Test]
+        public void ReceivePing()
         {
             // Setup
             Mock<IConnection> mockConnection = new Mock<IConnection>();
