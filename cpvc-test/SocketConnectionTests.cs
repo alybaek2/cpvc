0aaf5d79aad92e67f8c87e35956397796d71499e
adrian <alybaek2@gmail.com>
2020-06-09 00:45:15 -0700

    Adding another SocketConnection unit test.

--- cpvc-test/SocketConnectionTests.cs
+++ cpvc-test/SocketConnectionTests.cs
@@ -94,5 +94,37 @@ namespace CPvC.Test
             // Verify
             mockSocket.Verify(s => s.Close(), Times.Once());
         }
+
+        [Test]
+        public void Receive()
+        {
+            // Setup
+            System.Threading.ManualResetEvent e = new System.Threading.ManualResetEvent(true);
+            Mock<IAsyncResult> mockResult = new Mock<IAsyncResult>();
+            mockResult.SetupGet(r => r.AsyncWaitHandle).Returns(e);
+            AsyncCallback receiveCallback = null;
+            Mock<ISocket> mockSocket = new Mock<ISocket>();
+            mockSocket.Setup(s => s.BeginReceive(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<SocketFlags>(), It.IsAny<AsyncCallback>(), null)).Callback<byte[], int, int, SocketFlags, AsyncCallback, object>((b, offset, s, f, c, o) => {
+                // Request available machines...
+                b[0] = 0x05;
+                b[1] = 0xff;
+
+                receiveCallback = c;
+                });
+
+            mockSocket.SetupGet(s => s.Connected).Returns(true);
+            mockSocket.Setup(s => s.EndReceive(It.IsAny<IAsyncResult>())).Returns(2);
+
+            SocketConnection connection = new SocketConnection(mockSocket.Object);
+
+            Mock<NewMessageDelegate> mockNewMessage = new Mock<NewMessageDelegate>();
+            connection.OnNewMessage += mockNewMessage.Object;
+
+            // Act
+            receiveCallback?.Invoke(mockResult.Object);
+
+            // Verify
+            mockNewMessage.Verify(m => m(new byte[] { 0x05 }));
+        }
     }
 }
