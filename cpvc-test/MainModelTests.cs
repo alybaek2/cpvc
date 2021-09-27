709d7076c8df3e1f9a4c21472e1e643576d987ef
adrian <alybaek2@gmail.com>
2021-09-26 21:44:48 -0700

    Added unit tests for MainModel.

---
+++ cpvc-test/MainModelTests.cs
@@ -0,0 +1,79 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace CPvC.Test
+{
+    public class MainModelTests
+    {
+        Mock<IFileByteStream> _mockFileBytestream;
+        Mock<IFileSystem> _mockFileSystem;
+        Machine _machine;
+        Mock<ISettings> _mockSettings;
+        MainModel _mainModel;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockFileBytestream = new Mock<IFileByteStream>();
+            _mockFileSystem = new Mock<IFileSystem>();
+            _mockFileSystem.Setup(fs => fs.OpenFileByteStream("test.cpvc")).Returns(_mockFileBytestream.Object);
+            _machine = Machine.New("test", null);
+            _mockSettings = new Mock<ISettings>();
+            _mainModel = new MainModel(_mockSettings.Object, null);
+        }
+
+        [Test]
+        public void AddMachineUpdate()
+        {
+            // Act
+            _mainModel.AddMachine(_machine);
+
+            // Verify
+            _mockSettings.VerifySet(s => s.RecentlyOpened = It.IsAny<string>(), Times.Once);
+        }
+
+        [Test]
+        public void RemoveMachineUpdate()
+        {
+            // Setup
+            _mainModel.AddMachine(_machine);
+            _mockSettings.Invocations.Clear();
+
+            // Act
+            _mainModel.RemoveMachine(_machine);
+
+            // Verify
+            _mockSettings.VerifySet(s => s.RecentlyOpened = It.IsAny<string>(), Times.Once);
+        }
+
+        [Test]
+        public void ModifyMachineUpdate()
+        {
+            // Setup
+            _mainModel.AddMachine(_machine);
+            _mockSettings.Invocations.Clear();
+
+            // Act
+            _machine.Persist(_mockFileSystem.Object, "test.cpvc");
+
+            // Verify
+            _mockSettings.VerifySet(s => s.RecentlyOpened = It.IsAny<string>(), Times.Once);
+        }
+
+        [Test]
+        public void ModifyRemovedMachine()
+        {
+            // Setup
+            _mainModel.AddMachine(_machine);
+            _mainModel.RemoveMachine(_machine);
+            _mockSettings.Invocations.Clear();
+
+            // Act
+            _machine.Persist(_mockFileSystem.Object, "test.cpvc");
+
+            // Verify
+            _mockSettings.VerifySet(s => s.RecentlyOpened = It.IsAny<string>(), Times.Never);
+        }
+    }
+}
