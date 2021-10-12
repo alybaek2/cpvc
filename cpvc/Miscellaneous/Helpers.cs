13b536ebdad0fcf483ce7791b49f3ac3ca893e89
adrian <alybaek2@gmail.com>
2021-10-11 23:23:08 -0700

    Removed unneeded files.

--- cpvc/Miscellaneous/Helpers.cs
+++ cpvc/Miscellaneous/Helpers.cs
@@ -71,26 +71,6 @@ namespace CPvC
             }
         }
 
-        static public byte[] BinaryUndiff(byte[] reference, byte[] diff)
-        {
-            using (MemoryStream outStream = new MemoryStream())
-            {
-                deltaq.BsDiff.BsPatch.Apply(reference, diff, outStream);
-
-                return outStream.ToArray();
-            }
-        }
-
-        static public byte[] BinaryDiff(byte[] reference, byte[] blob)
-        {
-            using (MemoryStream outStream = new MemoryStream())
-            {
-                deltaq.BsDiff.BsDiff.Create(reference, blob, outStream);
-
-                return outStream.ToArray();
-            }
-        }
-
         /// <summary>
         /// Splits a string based on a delimiter, while ignoring delimiters escaped with a backspace.
         /// </summary>
