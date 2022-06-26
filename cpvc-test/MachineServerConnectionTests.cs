3598c84f8d71959c97a89cb12ba36655c9bfcec2
adrian <alybaek2@gmail.com>
2022-06-26 09:20:19 -0700

    Added back MachineServerConnectionTests.

--- cpvc-test/MachineServerConnectionTests.cs
+++ cpvc-test/MachineServerConnectionTests.cs
@@ -6,163 +6,167 @@ using System.Linq;
 
 namespace CPvC.Test
 {
-    //public class MachineServerConnectionTests
-    //{
-    //    private ReceivePingDelegate _receivePing;
-    //    private ReceiveRequestAvailableMachinesDelegate _receiveRequestAvailableMachines;
-    //    private ReceiveSelectMachineDelegate _receiveSelectMachine;
-    //    private ReceiveCoreRequestDelegate _receiveCoreAction;
+    public class MachineServerConnectionTests
+    {
+        private ReceivePingDelegate _receivePing;
+        private ReceiveRequestAvailableMachinesDelegate _receiveRequestAvailableMachines;
+        private ReceiveSelectMachineDelegate _receiveSelectMachine;
+        private ReceiveCoreRequestDelegate _receiveCoreAction;
 
-    //    private Mock<IRemote> _mockRemote;
-    //    private MachineServerConnection _serverConnection;
+        private Mock<IRemote> _mockRemote;
+        private MachineServerConnection _serverConnection;
 
-    //    private List<IMachine> _machines;
-    //    private List<Core> _cores;
+        private List<IMachine> _machines;
 
-    //    private Mock<IMachine>[] _mockMachines;
+        private Mock<IMachine>[] _mockMachines;
+        private MachineAuditorDelegate[] _auditorEvents;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        _cores = new List<Core>();
-    //        _mockMachines = new Mock<IMachine>[2];
+        [SetUp]
+        public void Setup()
+        {
+            //_cores = new List<Core>();
+            _mockMachines = new Mock<IMachine>[2];
+            _auditorEvents = new MachineAuditorDelegate[2];
 
-    //        for (int i = 0; i < _mockMachines.Length; i++)
-    //        {
-    //            Core core = new Core(Core.LatestVersion, Core.Type.CPC6128);
+            _mockMachines[0] = new Mock<IMachine>();
 
-    //            _cores.Add(core);
+            _mockMachines[0].SetupGet(m => m.Name).Returns("Machine0");
+            _mockMachines[0].SetupGet(m => m.Ticks).Returns(() => 1);
+            _mockMachines[0].SetupSet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>())
+                            .Callback<MachineAuditorDelegate>(callback =>
+                            {
+                                _auditorEvents[0] = callback;
+                            });
 
-    //            _mockMachines[i] = new Mock<IMachine>();
+            _mockMachines[1] = new Mock<IMachine>();
 
-    //            _mockMachines[i].SetupGet(m => m.Name).Returns(String.Format("Machine{0}", i));
-    //            _mockMachines[i].SetupGet(m => m.Core).Returns(core);
-    //            _mockMachines[i].SetupGet(m => m.Ticks).Returns(() => core.Ticks);
-    //            _mockMachines[i].SetupSet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>())
-    //                            .Callback<MachineAuditorDelegate>(callback => core.OnCoreAction += (sender, args) => callback(args.Action));
-    //        }
+            _mockMachines[1].SetupGet(m => m.Name).Returns("Machine1");
+            _mockMachines[1].SetupGet(m => m.Ticks).Returns(() => 1);
+            _mockMachines[1].SetupSet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>())
+                            .Callback<MachineAuditorDelegate>(callback =>
+                            {
+                                _auditorEvents[1] = callback;
+                            });
 
-    //        _machines = _mockMachines.Select(m => m.Object).ToList();
+            _machines = _mockMachines.Select(m => m.Object).ToList();
 
-    //        _mockRemote = new Mock<IRemote>();
+            _mockRemote = new Mock<IRemote>();
 
-    //        _receivePing = null;
-    //        _mockRemote.SetupSet(r => r.ReceivePing = It.IsAny<ReceivePingDelegate>()).Callback<ReceivePingDelegate>(callback => _receivePing = callback);
-    //        _mockRemote.SetupSet(r => r.ReceiveRequestAvailableMachines = It.IsAny<ReceiveRequestAvailableMachinesDelegate>()).Callback<ReceiveRequestAvailableMachinesDelegate>(callback => _receiveRequestAvailableMachines = callback);
-    //        _mockRemote.SetupSet(r => r.ReceiveSelectMachine = It.IsAny<ReceiveSelectMachineDelegate>()).Callback<ReceiveSelectMachineDelegate>(callback => _receiveSelectMachine = callback);
-    //        _mockRemote.SetupSet(r => r.ReceiveCoreRequest = It.IsAny<ReceiveCoreRequestDelegate>()).Callback<ReceiveCoreRequestDelegate>(callback => _receiveCoreAction = callback);
+            _receivePing = null;
+            _mockRemote.SetupSet(r => r.ReceivePing = It.IsAny<ReceivePingDelegate>()).Callback<ReceivePingDelegate>(callback => _receivePing = callback);
+            _mockRemote.SetupSet(r => r.ReceiveRequestAvailableMachines = It.IsAny<ReceiveRequestAvailableMachinesDelegate>()).Callback<ReceiveRequestAvailableMachinesDelegate>(callback => _receiveRequestAvailableMachines = callback);
+            _mockRemote.SetupSet(r => r.ReceiveSelectMachine = It.IsAny<ReceiveSelectMachineDelegate>()).Callback<ReceiveSelectMachineDelegate>(callback => _receiveSelectMachine = callback);
+            _mockRemote.SetupSet(r => r.ReceiveCoreRequest = It.IsAny<ReceiveCoreRequestDelegate>()).Callback<ReceiveCoreRequestDelegate>(callback => _receiveCoreAction = callback);
 
-    //        _serverConnection = new MachineServerConnection(_mockRemote.Object, _machines);
-    //    }
+            _serverConnection = new MachineServerConnection(_mockRemote.Object, _machines);
+        }
 
-    //    [TestCase(false)]
-    //    [TestCase(true)]
-    //    public void ReceivePing(bool response)
-    //    {
-    //        // Act
-    //        _receivePing(response, 1234);
+        [TestCase(false)]
+        [TestCase(true)]
+        public void ReceivePing(bool response)
+        {
+            // Act
+            _receivePing(response, 1234);
 
-    //        // Verify
-    //        _mockRemote.Verify(r => r.SendPing(true, 1234), Times.Exactly(response ? 0 : 1));
-    //    }
+            // Verify
+            _mockRemote.Verify(r => r.SendPing(true, 1234), Times.Exactly(response ? 0 : 1));
+        }
 
-    //    [Test]
-    //    public void ReceiveRequestAvailableMachines()
-    //    {
-    //        // Act
-    //        _receiveRequestAvailableMachines();
+        [Test]
+        public void ReceiveRequestAvailableMachines()
+        {
+            // Act
+            _receiveRequestAvailableMachines();
 
-    //        // Verify
-    //        IEnumerable<string> machineNames = _machines.Select(m => m.Name);
-    //        _mockRemote.Verify(r => r.SendAvailableMachines(machineNames));
-    //    }
+            // Verify
+            IEnumerable<string> machineNames = _machines.Select(m => m.Name);
+            _mockRemote.Verify(r => r.SendAvailableMachines(machineNames));
+        }
 
-    //    [Test]
-    //    public void ReceiveRequestNoAvailableMachines()
-    //    {
-    //        // Setup
-    //        _machines.Clear();
+        [Test]
+        public void ReceiveRequestNoAvailableMachines()
+        {
+            // Setup
+            _machines.Clear();
 
-    //        // Act
-    //        _receiveRequestAvailableMachines();
+            // Act
+            _receiveRequestAvailableMachines();
 
-    //        // Verify
-    //        _mockRemote.Verify(r => r.SendAvailableMachines(new List<string>()));
-    //    }
+            // Verify
+            _mockRemote.Verify(r => r.SendAvailableMachines(new List<string>()));
+        }
 
-    //    [TestCase(1)]
-    //    [TestCase(2)]
-    //    public void ReceiveSelectMachine(int selectCount)
-    //    {
-    //        // Act
-    //        for (int i = 0; i < selectCount; i++)
-    //        {
-    //            _receiveSelectMachine(_machines[0].Name);
-    //        }
+        [TestCase(1)]
+        [TestCase(2)]
+        public void ReceiveSelectMachine(int selectCount)
+        {
+            // Act
+            for (int i = 0; i < selectCount; i++)
+            {
+                _receiveSelectMachine(_machines[0].Name);
+            }
 
-    //        // Verify
-    //        _mockRemote.Verify(r => r.SendName(_machines[0].Name), Times.Once());
-    //        _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)), Times.Once());
-    //        _mockMachines[0].VerifySet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>(), Times.Once());
-    //    }
+            // Verify
+            _mockRemote.Verify(r => r.SendName(_machines[0].Name), Times.Once());
+            _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)), Times.Once());
+            _mockMachines[0].VerifySet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>(), Times.Once());
+        }
 
-    //    [Test]
-    //    public void ReceiveSelectDifferentMachines()
-    //    {
-    //        // Setup
-    //        _mockRemote.SetupSet(r => r.ReceiveSelectMachine = It.IsAny<ReceiveSelectMachineDelegate>());
-    //        _mockMachines[0].SetupSet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null));
-    //        _mockRemote.Setup(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)));
-    //        _mockRemote.Setup(r => r.SendName(_machines[1].Name));
-    //        _mockRemote.Setup(r => r.SendName(_machines[0].Name));
+        [Test]
+        public void ReceiveSelectDifferentMachines()
+        {
+            // Setup
+            _mockRemote.SetupSet(r => r.ReceiveSelectMachine = It.IsAny<ReceiveSelectMachineDelegate>());
+            _mockMachines[0].SetupSet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null));
+            _mockRemote.Setup(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)));
+            _mockRemote.Setup(r => r.SendName(_machines[1].Name));
+            _mockRemote.Setup(r => r.SendName(_machines[0].Name));
 
-    //        // Act
-    //        _receiveSelectMachine(_machines[0].Name);
-    //        _receiveSelectMachine(_machines[1].Name);
-    //        _receiveSelectMachine("UnknownMachine");
+            // Act
+            _receiveSelectMachine(_machines[0].Name);
+            _receiveSelectMachine(_machines[1].Name);
+            _receiveSelectMachine("UnknownMachine");
 
-    //        // Verify - Todo: verify the sequence of these calls.
-    //        _mockRemote.Verify(r => r.SendName(_machines[0].Name), Times.Once());
-    //        _mockRemote.Verify(r => r.SendName(_machines[1].Name), Times.Once());
-    //        _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)), Times.Exactly(2));
-    //        _mockMachines[0].VerifySet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null), Times.Once());
-    //        _mockMachines[0].VerifySet(m => m.Auditors = null, Times.Once());
-    //        _mockMachines[1].VerifySet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null), Times.Once());
-    //        _mockMachines[1].VerifySet(m => m.Auditors = null, Times.Once());
-    //    }
+            // Verify - Todo: verify the sequence of these calls.
+            _mockRemote.Verify(r => r.SendName(_machines[0].Name), Times.Once());
+            _mockRemote.Verify(r => r.SendName(_machines[1].Name), Times.Once());
+            _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)), Times.Exactly(2));
+            _mockMachines[0].VerifySet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null), Times.Once());
+            _mockMachines[0].VerifySet(m => m.Auditors = null, Times.Once());
+            _mockMachines[1].VerifySet(m => m.Auditors = It.Is<MachineAuditorDelegate>(d => d != null), Times.Once());
+            _mockMachines[1].VerifySet(m => m.Auditors = null, Times.Once());
+        }
 
-    //    //[Test]
-    //    //public void ReceiveCoreAction()
-    //    //{
-    //    //    // Setup
-    //    //    CoreAction coreAction = CoreAction.RunUntil(0, 1000, null);
+        [Test]
+        public void ReceiveCoreAction()
+        {
+            // Setup
+            CoreAction coreAction = CoreAction.RunUntil(0, 1000, null);
 
-    //    //    // Act
-    //    //    _receiveSelectMachine(_mockMachines[0].Object.Name);
-    //    //    _receiveCoreAction(coreAction);
+            // Act
+            _receiveSelectMachine(_mockMachines[0].Object.Name);
+            _receiveCoreAction(coreAction);
+            _auditorEvents[0](coreAction);
 
-    //    //    _cores[0].Start();
-    //    //    TestHelpers.ProcessOneRequest(_cores[0], coreAction);
+            // Verify
+            _mockRemote.Verify(r => r.SendName(_mockMachines[0].Object.Name));
+            _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)));
+            _mockMachines[0].VerifySet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>());
+        }
 
-    //    //    // Verify
-    //    //    Assert.AreEqual(1000, _machines[0].Ticks);
-    //    //    _mockRemote.Verify(r => r.SendName(_mockMachines[0].Object.Name));
-    //    //    _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.LoadCore)));
-    //    //    _mockMachines[0].VerifySet(m => m.Auditors = It.IsAny<MachineAuditorDelegate>());
-    //    //}
+        [Test]
+        public void MachineAuditor()
+        {
+            // Setup
+            _receiveSelectMachine(_machines[0].Name);
 
-    //    //[Test]
-    //    //public void MachineAuditor()
-    //    //{
-    //    //    // Setup
-    //    //    _receiveSelectMachine(_machines[0].Name);
+            // Act
+            //_cores[0].Start();
+            _auditorEvents[0](CoreAction.RunUntil(0, 1000, null)); //  CoreRequest.RunUntil(1000);
+            //TestHelpers.ProcessOneRequest(_cores[0], CoreRequest.RunUntil(_cores[0].Ticks + 1));
 
-    //    //    // Act
-    //    //    _cores[0].Start();
-    //    //    TestHelpers.ProcessOneRequest(_cores[0], CoreRequest.RunUntil(_cores[0].Ticks + 1));
-
-    //    //    // Verify
-    //    //    _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.RunUntil)));
-    //    //}
-    //}
+            // Verify
+            _mockRemote.Verify(r => r.SendCoreAction(It.Is<CoreAction>(a => a.Type == CoreRequest.Types.RunUntil)));
+        }
+    }
 }
