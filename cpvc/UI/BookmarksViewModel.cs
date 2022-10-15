8115e16cdbcf50490d4eb21eb872a51ebf520f1e
adrian <alybaek2@gmail.com>
2022-10-15 09:04:19 -0700

    Changed code to build history view.

--- cpvc/UI/BookmarksViewModel.cs
+++ cpvc/UI/BookmarksViewModel.cs
@@ -121,59 +121,7 @@ namespace CPvC.UI
             };
 
             // Note that items is sorted in ascending order of ticks (i.e. oldest to most recent).
-            List<HistoryViewItem> items = new List<HistoryViewItem>();
-
-            while (eventStack.Count > 0)
-            {
-                int left = eventStack[0].Item1;
-                HistoryEvent historyEvent = eventStack[0].Item2;
-                eventStack.RemoveAt(0);
-
-                if (ShouldShow(historyEvent))
-                {
-                    HistoryViewItem item = new HistoryViewItem(historyEvent);
-
-                    // Figure out where this new item should be placed.
-                    int itemIndex = items.FindIndex(x => x.HistoryEvent.Ticks > historyEvent.Ticks);
-                    if (itemIndex == -1)
-                    {
-                        // Not found? Add the item to the end.
-                        itemIndex = items.Count;
-                    }
-
-                    // Add passthrough events to all items inbetween the item and its parent.
-                    HistoryEvent parent = MostRecentShownAncestor(historyEvent);
-                    if (parent != null)
-                    {
-                        // As the parent should have been added by now, we don't need to check for FindIndex returning -1.
-                        int parentIndex = items.FindIndex(x => x.HistoryEvent == parent);
-                        for (int i = parentIndex + 1; i < itemIndex; i++)
-                        {
-                            left = items[i].AddEvent(left, historyEvent);
-                        }
-                    }
-
-                    // Copy the Events from the next item so passthroughs are correctly rendered.
-                    if (itemIndex < items.Count)
-                    {
-                        item.Events = new List<HistoryEvent>(items[itemIndex].Events);
-                    }
-
-                    // Now add the actual event itself.
-                    left = item.AddEvent(left, historyEvent);
-
-                    items.Insert(itemIndex, item);
-                }
-
-                List<HistoryEvent> sortedChildren = new List<HistoryEvent>(historyEvent.Children);
-                sortedChildren.Sort((x, y) => y.GetMaxDescendentTicks().CompareTo(x.GetMaxDescendentTicks()));
-
-                for (int c = 0; c < sortedChildren.Count; c++)
-                {
-                    // Place the children at the top of the stack; effectively means we're doing a depth-first walk of the tree.
-                    eventStack.Insert(c, new Tuple<int, HistoryEvent>(left, sortedChildren[c]));
-                }
-            }
+            List<HistoryViewItem> items = CreateHistoryView(_history);
 
             // Draw items to their respective canvasses.
             HistoryViewItem next = null;
@@ -193,6 +141,175 @@ namespace CPvC.UI
             {
                 Items.Add(item);
             }
+        }
+
+        static private bool InterestingEvent(HistoryEvent historyEvent)
+        {
+            bool interested = false;
+            if (historyEvent is RootHistoryEvent)
+            {
+                interested = true;
+            }
+            else if (historyEvent is BookmarkHistoryEvent)
+            {
+                interested = true;
+            }
+            else if (historyEvent.Children.Count != 1)
+            {
+                interested = true;
+            }
+
+            return interested;
+        }
+
+        static public List<HistoryViewItem> CreateHistoryView(History history)
+        {
+            List<HistoryEvent> verticalOrdering = new List<HistoryEvent>();
+
+            Dictionary<HistoryEvent, HistoryEvent> eventParents = new Dictionary<HistoryEvent, HistoryEvent>();
+
+            // Figure out vertical and horizontal ordering
+            Queue<HistoryEvent> historyEvents = new Queue<HistoryEvent>();
+            historyEvents.Enqueue(history.RootEvent);
+
+            while (historyEvents.Any())
+            {
+                HistoryEvent historyEvent = historyEvents.Dequeue();
+
+                if (InterestingEvent(historyEvent))
+                {
+                    // Get interesting parent
+                    HistoryEvent interestingParent = historyEvent.Parent;
+                    while (interestingParent != null && !InterestingEvent(interestingParent))
+                    {
+                        interestingParent = interestingParent.Parent;
+                    }
+
+                    eventParents.Add(historyEvent, interestingParent);
+                }
+
+                foreach (HistoryEvent childHistoryEvent in historyEvent.Children)
+                {
+                    historyEvents.Enqueue(childHistoryEvent);
+                }
+            }
+
+            Stack<HistoryEvent> historyEventsStack = new Stack<HistoryEvent>();
+            historyEventsStack.Push(history.RootEvent);
+
+            List<HistoryEvent> horizontalOrdering = new List<HistoryEvent>();
+
+            List<HistoryEvent> children = new List<HistoryEvent>();
+
+            while (historyEventsStack.Any())
+            {
+                HistoryEvent historyEvent = historyEventsStack.Pop();
+
+                children.Clear();
+                children.AddRange(historyEvent.Children);
+                children.Sort((x, y) => x.GetMaxDescendentTicks().CompareTo(y.GetMaxDescendentTicks()));
+
+                if (InterestingEvent(historyEvent))
+                {
+                    horizontalOrdering.Add(historyEvent);
+                }
+
+                foreach (HistoryEvent child in children)
+                {
+                    historyEventsStack.Push(child);
+                }
+            }
+
+            Dictionary<HistoryEvent, int> horizontalLookup = new Dictionary<HistoryEvent, int>();
+            Dictionary<HistoryEvent, int> verticalLookup = new Dictionary<HistoryEvent, int>();
+
+            verticalOrdering = new List<HistoryEvent>(horizontalOrdering);
+            verticalOrdering.Sort((x, y) =>
+            {
+                if (x.Ticks < y.Ticks)
+                {
+                    return -1;
+                }
+                else if (x.Ticks > y.Ticks)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if (x.IsEqualToOrAncestorOf(y))
+                    {
+                        return -1;
+                    }
+                    else if (y.IsEqualToOrAncestorOf(x))
+                    {
+                        return 1;
+                    }
+                }
+
+                return 0;
+            });
+
+            for (int i = 0; i < horizontalOrdering.Count; i++)
+            {
+                horizontalLookup[horizontalOrdering[i]] = i;
+            }
+
+            for (int i = 0; i < verticalOrdering.Count; i++)
+            {
+                verticalLookup[verticalOrdering[i]] = i;
+            }
+
+            List<HistoryViewItem> historyItems = new List<HistoryViewItem>();
+            HistoryViewItem previousViewItem = null;
+
+            for (int v = 0; v < verticalOrdering.Count; v++)
+            {
+                HistoryViewItem viewItem = new HistoryViewItem(verticalOrdering[v]);
+
+                // Add events; either "passthrough" events, or the actual event for this HistoryViewItem.
+                for (int h = 0; h < horizontalOrdering.Count; h++)
+                {
+                    int verticalHorizontalIndex = verticalLookup[horizontalOrdering[h]];
+                    int previousVerticalIndex = -1;
+
+                    if (eventParents.TryGetValue(horizontalOrdering[h], out HistoryEvent previousEvent) && previousEvent != null)
+                    {
+                        previousVerticalIndex = verticalLookup[previousEvent];
+                    }
+
+                    if (previousVerticalIndex < v && v <= verticalHorizontalIndex)
+                    {
+                        int hindex = viewItem.Events.Count;
+                        if (previousViewItem != null)
+                        {
+                            int prevIndex = previousViewItem.Events.FindIndex(x => x == horizontalOrdering[h]);
+                            if (prevIndex == -1)
+                            {
+                                prevIndex = previousViewItem.Events.FindIndex(x => x == previousEvent);
+                            }
+
+                            if (prevIndex != -1)
+                            {
+                                if (hindex < prevIndex)
+                                {
+                                    for (int g = 0; g < prevIndex - hindex; g++)
+                                    {
+                                        viewItem.Events.Add(null);
+                                    }
+                                }
+                            }
+                        }
+
+                        viewItem.Events.Add(horizontalOrdering[h]);
+                    }
+                }
+
+                historyItems.Add(viewItem);
+
+                previousViewItem = viewItem;
+            }
+
+            return historyItems;
         }
 
         private void DeleteBookmarks()
