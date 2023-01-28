009d97c0047ac9af6d775860a3e8020920748783
adrian <alybaek2@gmail.com>
2023-01-28 00:32:19 -0800

    Reused Ellipses and Polylines instead of creating new ones.

--- cpvc/UI/HistoryControl.cs
+++ cpvc/UI/HistoryControl.cs
@@ -169,48 +169,42 @@ namespace CPvC
                 ListTreeNode<HistoryEvent> node = kvp.Key;
                 Line line = kvp.Value;
 
+                Polyline polyline;
+                Ellipse circle;
                 if (!_linesToBranchShapes.TryGetValue(line, out BranchShapes bs))
                 {
+                    polyline = CreatePolyline();
+                    Canvas.SetZIndex(polyline, 1);
+                    Children.Add(polyline);
+
+                    circle = CreateCircle();
+                    Canvas.SetZIndex(circle, 100);
+                    Children.Add(circle);
+
                     bs = new BranchShapes();
                     _linesToBranchShapes.Add(line, bs);
+
+                    bs.Dot = circle;
+                    bs.Polyline = polyline;
                 }
                 else if (bs.LineVersion == line._version)
                 {
                     continue;
                 }
-
-                Polyline polyline = CreatePolyline(line);
-                Ellipse circle = CreateCircle(line._points.Last(), radius, line._current, line._type);
-
-                // Ensure the dot is always "on top".
-                Canvas.SetZIndex(polyline, 1);
-                Children.Add(polyline);
-
-                if (circle != null)
+                else
                 {
-                    Canvas.SetZIndex(circle, 100);
-                    Children.Add(circle);
+                    polyline = bs.Polyline;
+                    circle = bs.Dot;
                 }
 
-                Children.Remove(bs.Dot);
-                Children.Remove(bs.Polyline);
+                UpdatePolyline(polyline, line);
+                UpdateCircle(circle, line._points.Last(), radius, line._current, line._type);
 
-                bs.Dot = circle;
-                bs.Polyline = polyline;
                 bs.LineVersion = line._version;
             }
 
             // Delete non-existant lines!
-            List<Line> deleteLines = new List<Line>();
-            foreach (Line line in _linesToBranchShapes.Keys)
-            {
-                if (_nodesToLines.Values.Contains(line))
-                {
-                    continue;
-                }
-
-                deleteLines.Add(line);
-            }
+            List<Line> deleteLines = _linesToBranchShapes.Keys.Where(x => !_nodesToLines.Values.Contains(x)).ToList();
 
             foreach (Line line in deleteLines)
             {
@@ -229,13 +223,9 @@ namespace CPvC
         {
             _updateArgs = changeArgs;
             if (Interlocked.Exchange(ref _updatePending, 1) != 0)
-            //if (_updatePending)
             {
-                //_updateArgs = changeArgs;
                 return;
             }
-
-            //_updatePending = 1;
 
             DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher);
             timer.Interval = new TimeSpan(0, 0, 0, 0, 20);
@@ -243,14 +233,14 @@ namespace CPvC
             {
                 timer.Stop();
 
+                Interlocked.Exchange(ref _updatePending, 0);
+
                 Stopwatch sw = Stopwatch.StartNew();
                 UpdateCanvasListTree(_updateArgs);
                 _updateArgs = null;
                 sw.Stop();
 
                 CPvC.Diagnostics.Trace("Update items took {0}ms", sw.ElapsedMilliseconds);
-
-                Interlocked.Exchange(ref _updatePending, 0);
             };
 
             timer.Start();
@@ -261,124 +251,52 @@ namespace CPvC
             UpdateLines(changeArgs.HorizontalOrdering, changeArgs.VerticalOrdering);
 
             SyncLinesToShapes();
-
-            //Dictionary<ListTreeNode<HistoryEvent>, Line> lines = DrawLines(changeArgs.HorizontalOrdering, changeArgs.VerticalOrdering);
-
-            //Dictionary<ListTreeNode<HistoryEvent>, Line> newNodesToLines = new Dictionary<ListTreeNode<HistoryEvent>, Line>();
-            //Dictionary<Line, BranchShapes> newLinesToShapes = new Dictionary<Line, BranchShapes>();
-
-            //int reused = 0;
-            //double radius = 0.5 * _scalingX;
-
-            //foreach (KeyValuePair<ListTreeNode<HistoryEvent>, Line> kvp in lines)
-            //{
-            //    ListTreeNode<HistoryEvent> node = kvp.Key;
-            //    Line line = kvp.Value;
-
-            //    bool current = ReferenceEquals(_history?.CurrentEvent, node.HistoryEvent);
-
-            //    // Check the old ones!
-            //    if (_nodesToLines.TryGetValue(node, out Line oldLine))
-            //    {
-            //        if (line.IsSame(oldLine))
-            //        {
-            //            // Just use the old one
-            //            reused++;
-            //            newNodesToLines.Add(node, oldLine);
-            //            newLinesToShapes.Add(oldLine, _linesToBranchShapes[oldLine]);
-            //            continue;
-            //        }
-            //    }
-
-            //    // Ensure lines are never "on top" of dots.
-            //    Point lastPoint = line.LastPoint();
-
-            //    Polyline polyline = CreatePolyline(line);
-            //    Ellipse circle = CreateCircle(lastPoint, radius, current, line._type);
-
-            //    // Ensure the dot is always "on top".
-            //    Canvas.SetZIndex(polyline, 1);
-            //    Children.Add(polyline);
-
-            //    if (circle != null)
-            //    {
-            //        Canvas.SetZIndex(circle, 100);
-            //        Children.Add(circle);
-            //    }
-
-            //    Height = _scalingY * 2 * (_listTree?.VerticalOrdering().Count ?? 0);
-
-            //    BranchShapes bs = new BranchShapes();
-            //    bs.Dot = circle;
-            //    bs.Polyline = polyline;
-            //    newNodesToLines.Add(node, line);
-            //    newLinesToShapes.Add(line, bs);
-            //}
-
-            //// Remove any shapes!
-            //int del = 0;
-            //foreach (KeyValuePair<ListTreeNode<HistoryEvent>, Line> kvp in _nodesToLines)
-            //{
-            //    if (newNodesToLines.ContainsValue(kvp.Value))
-            //    {
-            //        continue;
-            //    }
-
-            //    if (_linesToBranchShapes.TryGetValue(kvp.Value, out BranchShapes bs))
-            //    {
-            //        //BranchShapes bs = kvp.Value.Item2;
-            //        Children.Remove(bs.Dot);
-            //        Children.Remove(bs.Polyline);
-
-            //        _linesToBranchShapes.Remove(kvp.Value);
-            //        del++;
-            //    }
-            //    else
-            //    {
-            //        throw new Exception();
-            //    }
-            //}
-
-            //CPvC.Diagnostics.Trace("UpdateCanvas: {0} reused {1} deleted {2} total", reused, del, lines.Count);
-            //_nodesToLines = newNodesToLines;
-            //_linesToBranchShapes = newLinesToShapes;
         }
 
-        private Ellipse CreateCircle(Point centre, double radius, bool current, LinePointType type)
+        private Ellipse CreateCircle()
+        {
+            Ellipse circle = new Ellipse
+            {
+                Stroke = Brushes.DarkBlue,
+                Fill = Brushes.DarkBlue,
+                StrokeThickness = 2,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                UseLayoutRounding = true,
+                Margin = new Thickness(0, 0, 0, 0),
+                Width = 0,
+                Height = 0,
+                Visibility = Visibility.Collapsed
+            };
+
+            return circle;
+        }
+
+        private void UpdateCircle(Ellipse circle, Point centre, double radius, bool current, LinePointType type)
         {
             Brush brush;
             switch (type)
             {
-                case LinePointType.None:
-                    return null;
                 case LinePointType.SystemBookmark:
                     brush = Brushes.DarkRed;
                     break;
                 case LinePointType.UserBookmark:
-                    brush = Brushes.Red;
+                    brush = Brushes.Crimson;
                     break;
                 default:
                     brush = Brushes.DarkBlue;
                     break;
             }
 
-            Ellipse circle = new Ellipse
-            {
-                Stroke = brush,
-                Fill = current ? Brushes.White : brush,
-                StrokeThickness = 2,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Top,
-                UseLayoutRounding = true,
-                Margin = new Thickness(centre.X - radius, centre.Y - radius, 0, 0),
-                Width = 2 * radius,
-                Height = 2 * radius
-            };
-
-            return circle;
+            circle.Stroke = brush;
+            circle.Fill = current ? Brushes.White : brush;
+            circle.Margin = new Thickness(centre.X - radius, centre.Y - radius, 0, 0);
+            circle.Width = 2 * radius;
+            circle.Height = 2 * radius;
+            circle.Visibility = (type == LinePointType.None) ? Visibility.Collapsed : Visibility.Visible;
         }
 
-        private Polyline CreatePolyline(Line line)
+        private Polyline CreatePolyline()
         {
             Polyline polyline = new Polyline
             {
@@ -389,19 +307,48 @@ namespace CPvC
                 UseLayoutRounding = true
             };
 
+            return polyline;
+        }
+
+        private void UpdatePolyline(Polyline polyline, Line line)
+        {
+            void AddPoint(PointCollection pointCollection, int index, int x, int y)
+            {
+                if (index < pointCollection.Count)
+                {
+                    if (pointCollection[index].X != x || pointCollection[index].Y != y)
+                    {
+                        pointCollection[index] = new System.Windows.Point(x, y);
+                    }
+                }
+                else
+                {
+                    pointCollection.Add(new System.Windows.Point(x, y));
+                }
+            }
+
+            int pp = 0;
             int lastX = -1;
             for (int pindex = 0; pindex < line._points.Count; pindex++)
             {
+                Point point = line._points[pindex];
                 if (lastX >= 0)
                 {
-                    polyline.Points.Add(new System.Windows.Point(line._points[pindex].X, line._points[pindex].Y - 1 * _scalingY));
+                    AddPoint(polyline.Points, pp, point.X, point.Y - 1 * _scalingY);
+                    pp++;
                 }
-                polyline.Points.Add(new System.Windows.Point(line._points[pindex].X, line._points[pindex].Y));
+
+                AddPoint(polyline.Points, pp, point.X, point.Y);
+                pp++;
 
                 lastX = line._points[pindex].X;
             }
 
-            return polyline;
+            // Trim any extra points.
+            while (polyline.Points.Count > pp)
+            {
+                polyline.Points.RemoveAt(pp);
+            }
         }
 
         private class BranchShapes
