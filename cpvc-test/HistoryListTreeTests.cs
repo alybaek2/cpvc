a3cc8200491311d67743312d993ae6f6f1f4ca46
adrian <alybaek2@gmail.com>
2023-03-27 01:20:01 -0700

    Added another unit test.

--- cpvc-test/HistoryListTreeTests.cs
+++ cpvc-test/HistoryListTreeTests.cs
@@ -21,17 +21,43 @@ namespace CPvC.Test
             HistoryEvent event1 = history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
 
             // Verify
-            IEnumerable<HistoryEvent> horizontalEvents = historyListTree.HorizontalOrdering().Select(x => x.Data);
-            IEnumerable<HistoryEvent> verticalEvents = historyListTree.VerticalOrdering().Select(x => x.Data);
-
             List<HistoryEvent> expectedOrdering = new List<HistoryEvent>
             {
                 history.RootEvent,
                 event1
             };
 
-            Assert.AreEqual(expectedOrdering, horizontalEvents);
-            Assert.AreEqual(expectedOrdering, verticalEvents);
+            VerifyOrderings(expectedOrdering, expectedOrdering, historyListTree);
+        }
+
+        [Test]
+        public void AddHistoryEvents()
+        {
+            // Setup
+            History history = new History();
+            HistoryListTree historyListTree = new HistoryListTree(history);
+
+            // Act
+            HistoryEvent event1 = history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
+            HistoryEvent event2 = history.AddCoreAction(new KeyPressAction(200, Keys.A, false));
+
+            // Verify
+            List<HistoryEvent> expectedOrdering = new List<HistoryEvent>
+            {
+                history.RootEvent,
+                event2
+            };
+
+            VerifyOrderings(expectedOrdering, expectedOrdering, historyListTree);
+        }
+
+        private void VerifyOrderings(IEnumerable<HistoryEvent> expectedHorizontalOrdering, IEnumerable<HistoryEvent> expectedVerticalOrdering, HistoryListTree historyListTree)
+        {
+            IEnumerable<HistoryEvent> horizontalEvents = historyListTree.HorizontalOrdering().Select(x => x.Data);
+            IEnumerable<HistoryEvent> verticalEvents = historyListTree.VerticalOrdering().Select(x => x.Data);
+
+            Assert.AreEqual(expectedHorizontalOrdering, horizontalEvents);
+            Assert.AreEqual(expectedVerticalOrdering, verticalEvents);
         }
     }
 }
