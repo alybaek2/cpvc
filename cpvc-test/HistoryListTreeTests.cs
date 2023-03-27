243b025938770ecee4ba9416a5af42e4f49a21db
adrian <alybaek2@gmail.com>
2023-03-27 01:00:42 -0700

    Added unit test for HistoryListTree.

---
+++ cpvc-test/HistoryListTreeTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPvC.Test
+{
+    public class HistoryListTreeTests
+    {
+        [Test]
+        public void AddHistoryEvent()
+        {
+            // Setup
+            History history = new History();
+            HistoryListTree historyListTree = new HistoryListTree(history);
+
+            // Act
+            HistoryEvent event1 = history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
+
+            // Verify
+            IEnumerable<HistoryEvent> horizontalEvents = historyListTree.HorizontalOrdering().Select(x => x.Data);
+            IEnumerable<HistoryEvent> verticalEvents = historyListTree.VerticalOrdering().Select(x => x.Data);
+
+            List<HistoryEvent> expectedOrdering = new List<HistoryEvent>
+            {
+                history.RootEvent,
+                event1
+            };
+
+            Assert.AreEqual(expectedOrdering, horizontalEvents);
+            Assert.AreEqual(expectedOrdering, verticalEvents);
+        }
+    }
+}
