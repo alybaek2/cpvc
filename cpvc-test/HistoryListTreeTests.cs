9c872141b71cee86bb1a4dbc8cc4cfb74f975da7
adrian <alybaek2@gmail.com>
2023-03-27 22:33:32 -0700

    Simplified unit test verification. Also added a few more tests.

--- cpvc-test/HistoryListTreeTests.cs
+++ cpvc-test/HistoryListTreeTests.cs
@@ -10,54 +10,120 @@ namespace CPvC.Test
 {
     public class HistoryListTreeTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            _history = new History();
+            _historyListTree = new HistoryListTree(_history);
+        }
+
         [Test]
         public void AddHistoryEvent()
         {
-            // Setup
-            History history = new History();
-            HistoryListTree historyListTree = new HistoryListTree(history);
-
             // Act
-            HistoryEvent event1 = history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
+            HistoryEvent event1 = _history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
 
             // Verify
-            List<HistoryEvent> expectedOrdering = new List<HistoryEvent>
-            {
-                history.RootEvent,
-                event1
-            };
-
-            VerifyOrderings(expectedOrdering, expectedOrdering, historyListTree);
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event1);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event1);
         }
 
         [Test]
         public void AddHistoryEvents()
         {
-            // Setup
-            History history = new History();
-            HistoryListTree historyListTree = new HistoryListTree(history);
-
             // Act
-            HistoryEvent event1 = history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
-            HistoryEvent event2 = history.AddCoreAction(new KeyPressAction(200, Keys.A, false));
+            HistoryEvent event1 = _history.AddCoreAction(new KeyPressAction(100, Keys.A, true));
+            HistoryEvent event2 = _history.AddCoreAction(new KeyPressAction(200, Keys.A, false));
 
             // Verify
-            List<HistoryEvent> expectedOrdering = new List<HistoryEvent>
-            {
-                history.RootEvent,
-                event2
-            };
-
-            VerifyOrderings(expectedOrdering, expectedOrdering, historyListTree);
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event2);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event2);
         }
 
-        private void VerifyOrderings(IEnumerable<HistoryEvent> expectedHorizontalOrdering, IEnumerable<HistoryEvent> expectedVerticalOrdering, HistoryListTree historyListTree)
+        [Test]
+        public void AddBranchedEvent()
         {
-            IEnumerable<HistoryEvent> horizontalEvents = historyListTree.HorizontalOrdering().Select(x => x.Data);
-            IEnumerable<HistoryEvent> verticalEvents = historyListTree.VerticalOrdering().Select(x => x.Data);
+            // Act
+            HistoryEvent event1 = _history.AddCoreAction(new RunUntilAction(100, 200, null));
+            HistoryEvent event2 = _history.AddBookmark(200, null);
+            HistoryEvent event3 = _history.AddCoreAction(new KeyPressAction(400, Keys.A, true));
+            _history.CurrentEvent = event2;
+            HistoryEvent event4 = _history.AddCoreAction(new KeyPressAction(300, Keys.B, true));
 
-            Assert.AreEqual(expectedHorizontalOrdering, horizontalEvents);
-            Assert.AreEqual(expectedVerticalOrdering, verticalEvents);
+            // Verify
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event2, event3, event4);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event2, event4, event3);
         }
+
+        [Test]
+        public void AddBranchedEventTie()
+        {
+            // Act
+            HistoryEvent event1 = _history.AddCoreAction(new RunUntilAction(100, 200, null));
+            HistoryEvent event2 = _history.AddBookmark(200, null);
+            HistoryEvent event3 = _history.AddCoreAction(new KeyPressAction(300, Keys.A, true));
+            _history.CurrentEvent = event2;
+            HistoryEvent event4 = _history.AddCoreAction(new RunUntilAction(200, 300, null));
+
+            // Verify - note the Id is used in the event of a tie.
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event2, event4, event3);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event2, event3, event4);
+        }
+
+        [Test]
+        public void AddBranchedEventBreakTie1()
+        {
+            // Act
+            HistoryEvent event1 = _history.AddCoreAction(new RunUntilAction(100, 200, null));
+            HistoryEvent event2 = _history.AddBookmark(200, null);
+            HistoryEvent event3 = _history.AddCoreAction(new KeyPressAction(300, Keys.A, true));
+            _history.CurrentEvent = event2;
+            HistoryEvent event4 = _history.AddCoreAction(new RunUntilAction(200, 300, null));
+            HistoryEvent event5 = _history.AddCoreAction(new RunUntilAction(300, 304, null));
+
+            // Verify - note the Id is used in the event of a tie.
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event2, event5, event3);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event2, event3, event5);
+        }
+
+        [Test]
+        public void DeleteBookmarkParentBecomesVisible()
+        {
+            // Act
+            HistoryEvent event1 = _history.AddCoreAction(new RunUntilAction(100, 200, null));
+            HistoryEvent event2 = _history.AddBookmark(200, null);
+            HistoryEvent event3 = _history.AddCoreAction(new KeyPressAction(300, Keys.A, true));
+            _history.CurrentEvent = event2;
+            HistoryEvent event4 = _history.AddCoreAction(new RunUntilAction(200, 400, null));
+            _history.DeleteBookmark(event2);
+
+            // Verify - note the Id is used in the event of a tie.
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event1, event4, event3);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event1, event3, event4);
+        }
+
+        [Test]
+        public void DeleteBookmarkParentAlreadyVisible()
+        {
+            // Act
+            HistoryEvent event1 = _history.AddBookmark(100, null);
+            HistoryEvent event2 = _history.AddBookmark(200, null);
+            HistoryEvent event3 = _history.AddCoreAction(new KeyPressAction(300, Keys.A, true));
+            _history.CurrentEvent = event2;
+            HistoryEvent event4 = _history.AddCoreAction(new RunUntilAction(200, 400, null));
+            _history.DeleteBookmark(event2);
+
+            // Verify - note the Id is used in the event of a tie.
+            VerifyOrderings(_historyListTree.HorizontalOrdering(), _history.RootEvent, event1, event4, event3);
+            VerifyOrderings(_historyListTree.VerticalOrdering(), _history.RootEvent, event1, event3, event4);
+        }
+
+        private void VerifyOrderings(List<ListTreeNode<HistoryEvent>> actualOrdering, params object[] expectedOrdering)
+        {
+            Assert.AreEqual(actualOrdering.Select(x => x.Data), expectedOrdering);
+        }
+
+        private History _history;
+        private HistoryListTree _historyListTree;
     }
 }
