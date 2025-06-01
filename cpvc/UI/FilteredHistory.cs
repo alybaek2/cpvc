47096f6c32a319f4e4d63e573faf15bd06a7b42b
adrian <alybaek2@gmail.com>
2025-05-31 21:14:22 -0700

    Added a stub for the FilteredHistory class and ListTreeControl as a submodule..

---
+++ cpvc/UI/FilteredHistory.cs
@@ -0,0 +1,33 @@
+﻿using ListTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPvC
+{
+    public class FilteredHistory : BasicListTree
+    {
+        public FilteredHistory(History history) : base(history.RootEvent)
+        {
+            _history = history;
+
+            Sync();
+
+            _history.Auditors += History_Event;
+        }
+
+        private void Sync()
+        {
+
+        }
+
+        private void History_Event(object sender, HistoryChangedEventArgs e)
+        {
+
+        }
+
+        private History _history;
+    }
+}
